0ebfd1f7007e013147f1d60c5bd550f2cf4c7912
jelockro <jlockrow@gmail.com>
2019-06-03 08:49:38 -0500

    updated predict and train

--- predict.py
+++ predict.py
@@ -1,12 +1,10 @@
-% matplotlib
-inline
-% config
-InlineBackend.figure_format = 'retina'
+import matplotlib as mpl
+mpl.use('tkagg')
 import matplotlib.pyplot as plt
-
 import numpy as np
 import torch
-
+import argparse
+from process_image import process_image
 import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 
@@ -27,6 +25,27 @@ from torchvision import datasets, transforms, models
 #
 # Use GPU for inference:
 # python predict.py /path/to/image checkpoint --gpu
+def load_model_checkpoint(checkpoint):
+    # TODO: Write a function that loads a checkpoint and rebuilds the model
+    from torchvision import datasets, transforms, models
+    def rebuild(checkpoint):
+        #model = models.vgg11(pretrained=True)
+        model.classifier = nn.Sequential(nn.Linear(512 * 7 * 7, 4000),
+                                         nn.ReLU(),
+                                         # nn.Dropout(0.2),
+                                         nn.Linear(4000, 1280),
+                                         nn.ReLU(),
+                                         nn.Linear(1280, 102),
+                                         nn.LogSoftmax(dim=1))
+        state_dict = torch.load(checkpoint)
+        model.class_to_idx = train_data.class_to_idx
+        print(model.class_to_idx)
+        model.load_state_dict(state_dict)
+        print(model)
+        return model
+
+    loadedModel = rebuild('checkpoint2.pth')
+    return loadedModel
 
 
 def imshow(image, ax=None, title=None):
@@ -48,7 +67,7 @@ def imshow(image, ax=None, title=None):
     image = np.clip(image, 0, 1)
 
     ax.imshow(image)
-
+    plt.show()
     return fig, (ax, ax2)
 
 def predict(image_path, model, topk=5):
@@ -62,47 +81,78 @@ def predict(image_path, model, topk=5):
     #print(top_ps[0].tolist(), top_class.shape)
     return top_ps[0].tolist(), top_class[0].tolist()
     # TODO: Implement the code to predict the class from an image file
-predict('flowers/test/1/image_06743.flowers', loadedModel , topk=5)
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
+
+
+        # # instantiate the stacked plots
+        #fig, (ax1, ax2) = plt.subplots(2)
+
+        #path = 'flowers/test/1/image_06743.flowers'
+        path = 'flowers/test/1/image_06764.flowers'
+        loadedmodel = load_model_checkpoint(args.checkpoint)
+        top5_probs, top5_class_names = predict(path, loadedModel,5)
+        #print(top5_probs)
+        #print(top5_class_names)
+
+        flower_tensor_image = process_image(path)
+        # = torch.from_numpy(flower_np_image).type(torch.cuda.FloatTensor)
+        #flower_tensor_image = flower_tensor_image.unsqueeze_(0)
+
+        # make the first plot the image ax from imshow
+        fig, (axs, ax2) = imshow(flower_tensor_image)
+        axs.axis('off')
+        index = str(top5_class_names[0])
+        #print(index)
+        #print(cat_to_name[index].upper())
+        axs.set_title(cat_to_name[index].upper())
+        list_of_names =[]
+        for i in range(len(top5_class_names)):
+            list_of_names.append(cat_to_name[str(top5_class_names[i])])
+
+            #
+            #plt.figure(figsize=(1,1))
+            y_pos = np.arange(len(top5_class_names))
+            ax2.barh(y_pos, list(reversed(top5_probs)))
+            plt.yticks(y_pos, list(reversed(list_of_names)))
+            print(type(ax2))
+            print(type(axs))
+            ax2.axis('auto')
+            #plt.ylabel('Flower Type')
+            #plt.xlabel('Class Probability')
+            plt.show()
+#predict('flowers/test/1/image_06743.flowers', model , topk=5)
 
 # TODO: Display an image along with the top 5 classes
-import json
-
-with open('cat_to_name.json', 'r') as f:
-    cat_to_name = json.load(f)
 
 
-# # instantiate the stacked plots
-#fig, (ax1, ax2) = plt.subplots(2)
-
-#path = 'flowers/test/1/image_06743.flowers'
-path = 'flowers/test/1/image_06764.flowers'
-top5_probs, top5_class_names = predict(path, loadedModel,5)
-#print(top5_probs)
-#print(top5_class_names)
-
-flower_tensor_image = process_image(path)
-# = torch.from_numpy(flower_np_image).type(torch.cuda.FloatTensor)
-#flower_tensor_image = flower_tensor_image.unsqueeze_(0)
-
-# make the first plot the image ax from imshow
-fig, (axs, ax2) = imshow(flower_tensor_image)
-axs.axis('off')
-index = str(top5_class_names[0])
-#print(index)
-#print(cat_to_name[index].upper())
-axs.set_title(cat_to_name[index].upper())
-list_of_names =[]
-for i in range(len(top5_class_names)):
-    list_of_names.append(cat_to_name[str(top5_class_names[i])])
-
+####################
+# Examples of usage
+####################
 #
-#plt.figure(figsize=(1,1))
-y_pos = np.arange(len(top5_class_names))
-ax2.barh(y_pos, list(reversed(top5_probs)))
-plt.yticks(y_pos, list(reversed(list_of_names)))
-print(type(ax2))
-print(type(axs))
-ax2.axis('auto')
-#plt.ylabel('Flower Type')
-#plt.xlabel('Class Probability')
-#plt.show()
+# Return top K most likely classes
+# python predict.py /path/to/image checkpoint --top_k 3
+#
+# Use a mapping of categories to real names
+# python predict.py /path/to/image checkpoint --category_names cat_to_name.json
+#
+# Use GPU for inference:
+# python predict.py /path/to/image checkpoint --gpu
+
+# ex python predict.py flowers/test/1/image_06743.jpg checkpoint.pth
+def main():
+    parser = argparse.ArgumentParser(description="Predicts image based on loaded model")
+    parser.add_argument("image_path", help="path to image")
+    parser.add_argument("checkpoint", help="path to checkpoint")
+    parser.add_argument("--category_names", help="file that contains category name map")
+    parser.add_argument("--gpu", action='store_true', help="Turn on Cuda Usage")
+    parser.add_argument("--topk", type=int , help="Choose how many predictions")
+    args = parser.parse_args()
+    tensor_image = process_image(args.image_path)
+    imshow(tensor_image)
+    import json
+
+    
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 # Imports here
-import matplotlib.pyplot as plt
 import numpy as np
 from torch import nn
 from torch import optim
@@ -61,18 +60,54 @@ class Model:
     def setDevice(self):
         if self.gpu and torch.cuda.is_available():
             self.device = torch.device("cuda")
+            
         else:
             self.device = torch.device("cpu")
+        print('device set to {}'.format(self.device))
+        
         return self.device
+    
+    def setModel(self, arch):
+        print("\nsetting up model...\n")
+#         alexnet = models.alexnet(pretrained=True)
+#         squeezenet = models.squeezenet1_0(pretrained=True)
+#         vgg11 = models.vgg11(pretrained=True)
+#         vgg13 = models.vgg13(pretrained=True)
+#         vgg16 = models.vgg16(pretrained=True)
+#         vgg19 = models.vgg19(pretrained=True)
+#         densenet = models.densenet161(pretrained=True)
+#         inception = models.inception_v3(pretrained=True)
+#         googlenet = models.googlenet(pretrained=False)
+#         shufflenet = models.shufflenet_v2_x1_0(pretrained=True)
+#         mobilenet = models.mobilenet_v2(pretrained=True)
+#         resnext50_32x4d = models.resnext50_32x4d(pretrained=True)
+        switcher = {
+            'alexnet' : models.alexnet(pretrained=True),
+            'squeezenet': models.squeezenet1_0(pretrained=True),
+            'vgg16': models.vgg16(pretrained=True),
+            'inception' : models.inception_v3(pretrained=True),
+            #'googlenet': models.googlenet(pretrained=False),
+            #'mobilenet': models.mobilenet_v2(pretrained=True),
+            #'resnext50_32x4d' : models.resnext50_32x4d(pretrained=True),
+            'vgg11': models.vgg11(pretrained=True),
+            'vgg13': models.vgg13(pretrained=True),
+            'vgg16': models.vgg16(pretrained=True),
 
-    def setModel(self):
-        self.model = models.vgg11(pretrained=True)
-        return self.model
+        }
+        error = "\nThat model is not supported yet. The supported models are : 'alexnet', squeezenet',\
+        'vgg11', 'vgg13', 'vgg16', 'vgg19', 'inception', 'googlenet', 'mobilenet', 'resnext50_32x4d' "
+        self.model = switcher.get(arch, error)
+        if self.model == error:
+            print(error)
+        else: 
+            print('\nmodel successfully set to {}'.format(arch))
+        
+        
 
     # Freeze parameters so we don't backprop through them
 
     def create_classifier(self):
-
+        print("\ncreating classifier...")
         for param in self.model.parameters():
             param.requires_grad = False
 
@@ -84,139 +119,133 @@ class Model:
                                          nn.Linear(1280, 102),
                                          nn.LogSoftmax(dim=1))
 
-        criterion = nn.NLLLoss()
+        self.criterion = nn.NLLLoss()
 
         # Only train the classifier parameters, feature parameters are frozen
-        optimizer = optim.Adam(self.model.classifier.parameters(), lr=0.003)
-        optimizer.zero_grad()
+        self.optimizer = optim.Adam(self.model.classifier.parameters(), lr=0.003)
+        #optimizer.zero_grad()
         images, labels = next(iter(self.validloader))
-        ### Validate that the images are the right size
-        print(images[0,:].size())
-        print(images.size())
-        print(labels[0])
-
-        # print('labels', labels)
-        # print (images.size())
-        # images = images[1:3]
-        # print('images', images.size())
-        # images = images.view(1, -1)
-
-        print(images.size())
-        images = images.view(images.size(0), -1)
-        print(images.size())
         ps = torch.exp(self.model(images))
-        print("shape should be [64, 102]", ps.shape)
+        #print("shape should be [64, 102]", ps.shape)
         top_p, top_class = ps.topk(1, dim=1)
         print(top_class[:10, :])
         equals = top_class == labels.view(*top_class.shape)
         accuracy = torch.mean(equals.type(torch.FloatTensor))
         print(f'Accuracy: {accuracy.item() * 100}%')
-
-    #
-    # def train_model(self, model):
-    #     device = self.setDevice()
-    #     model.to(device);
-    #     epochs = 30
-    #     steps = 0
-    #
-    #     train_losses, test_losses = [], []
-    #     for epoch in range(epochs):
-    #         running_loss = 0
-    #         for inputs, labels in self.trainloader:
-    #
-    #             inputs, labels = inputs.to(self.device), labels.to(self.device)
-    #
-    #             optimizer.zero_grad()
-    #
-    #             logps = model.forward(inputs)
-    #             loss = criterion(logps, labels)
-    #             loss.backward()
-    #             optimizer.step()
-    #
-    #             running_loss += loss.item()
-    #         else:
-    #             ## TODO: Implement the validation pass and print out the validation accuracy
-    #             test_loss = 0
-    #             accuracy = 0
-    #
-    #             # Turn off gradients for validation, saves memory and computations
-    #             with torch.no_grad():
-    #                 for images, labels in validloader:
-    #                     images, labels = images.to(device), labels.to(device)
-    #                     log_ps = model(images)
-    #                     batch_loss = criterion(log_ps, labels)
-    #
-    #                     test_loss += batch_loss.item()
-    #
-    #                     ps = torch.exp(log_ps)
-    #                     top_p, top_class = ps.topk(1, dim=1)
-    #                     equals = top_class == labels.view(*top_class.shape)
-    #                     accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-    #
-    #             train_losses.append(running_loss / len(trainloader))
-    #             test_losses.append(test_loss / len(validloader))
-    #
-    #             print("Epoch: {}/{}.. ".format(epoch + 1, epochs),
-    #                   "Training Loss: {:.3f}.. ".format(running_loss / len(trainloader)),
-    #                   "Test Loss: {:.3f}.. ".format(test_loss / len(validloader)),
-    #                   "Test Accuracy: {:.3f}".format(accuracy / len(validloader)))
-    #
+        print('\ncheck results to see if classifer is configured correctly.')
+    
+    def train_model(self):
+        
+        device = self.setDevice()
+        self.model.to(self.device);
+        print('\ntraining {} on {}, for {} epochs. Optimizer learning rate set to {}...'.format(self.arch, self.device, self.epochs, self.learning_rate))
+        epochs = self.epochs
+        steps = 0
+        criterion = nn.NLLLoss()
+        optimizer = optim.Adam(self.model.classifier.parameters(), lr=self.learning_rate)
+        train_losses, test_losses = [], []
+        for epoch in range(epochs):
+            running_loss = 0
+            for inputs, labels in self.trainloader:
+    
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+    
+                optimizer.zero_grad()
+    
+                logps = self.model.forward(inputs)
+                loss = criterion(logps, labels)
+                loss.backward()
+                optimizer.step()
+    
+                running_loss += loss.item()
+            else:
+                ## TODO: Implement the validation pass and print out the validation accuracy
+                test_loss = 0
+                accuracy = 0
+    
+                # Turn off gradients for validation, saves memory and computations
+                with torch.no_grad():
+                    for images, labels in self.validloader:
+                        images, labels = images.to(self.device), labels.to(self.device)
+                        log_ps = self.model(images)
+                        batch_loss = criterion(log_ps, labels)
+    
+                        test_loss += batch_loss.item()
+    
+                        ps = torch.exp(log_ps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+    
+                train_losses.append(running_loss / len(self.trainloader))
+                test_losses.append(test_loss / len(self.validloader))
+    
+                print("Epoch: {}/{}.. ".format(epoch + 1, epochs),
+                      "Training Loss: {:.3f}.. ".format(running_loss / len(self.trainloader)),
+                      "Test Loss: {:.3f}.. ".format(test_loss / len(self.validloader)),
+                      "Test Accuracy: {:.3f}".format(accuracy / len(self.validloader)))
+    
     #
     # def plot_test(self, train_losses, test_losses):
     #     plt.plot(train_losses, label='Training loss')
     #     plt.plot(test_losses, label='Validation loss')
     #     plt.legend(frameon=False)
     #
-    # def validate_model(self, testloader, model):
-    #     test_loss = 0
-    #     accuracy = 0
+    def validate_model(self):
+        test_loss = 0
+        accuracy = 0
+        test_losses = []
+        # Turn off gradients for validation, saves memory and computations
+        with torch.no_grad():
+            for images, labels in self.testloader:
+                images, labels = images.to(self.device), labels.to(self.device)
+                log_ps = self.model(images)
+                batch_loss = self.criterion(log_ps, labels)
+    
+                test_loss += batch_loss.item()
+    
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+    
+        test_losses.append(test_loss / len(self.testloader))
+        print("Test Loss: {:.3f}.. ".format(test_loss / len(self.testloader)),
+        "Test Accuracy: {:.3f}".format(accuracy / len(self.testloader))
+             )
     #
-    #     # Turn off gradients for validation, saves memory and computations
-    #     with torch.no_grad():
-    #         for images, labels in testloader:
-    #             images, labels = images.to(device), labels.to(device)
-    #             log_ps = model(images)
-    #             batch_loss = criterion(log_ps, labels)
+    def save_model_checkpoint(self):
+        print("\nOur model: \n\n", self.model, '\n')
+        print("The state dict keys: \n\n", self.model.state_dict().keys())
+        checkpoint = self.save_dir + '/' + 'checkpoint2.pth'
+        print('\n\nsaving to {}.'.format(checkpoint))
+        try:
+            torch.save(self.model.state_dict(), checkpoint)
+        except:
+            print('Checkpoint did not save.')
+        print('Checkpoint successful.')
     #
-    #             test_loss += batch_loss.item()
-    #
-    #             ps = torch.exp(log_ps)
-    #             top_p, top_class = ps.topk(1, dim=1)
-    #             equals = top_class == labels.view(*top_class.shape)
-    #             accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-    #
-    #     test_loss.append(test_loss / len(validloader))
-    #
-    #     "Test Loss: {:.3f}.. ".format(test_loss / len(testloader)),
-    #     "Test Accuracy: {:.3f}".format(accuracy / len(testloader))
-    #
-    # def save_model_checkpoint(self, model):
-    #     print("Our model: \n\n", model, '\n')
-    #     print("The state dict keys: \n\n", model.state_dict().keys())
-    #
-    #     torch.save(model.state_dict(), 'checkpoint.pth')
-    #
-    # def load_model_checkpoint(self, checkpoint):
-    #     # TODO: Write a function that loads a checkpoint and rebuilds the model
-    #     from torchvision import datasets, transforms, models
-    #     def rebuild(checkpoint):
-    #         model = models.vgg11(pretrained=True)
-    #         model.classifier = nn.Sequential(nn.Linear(512 * 7 * 7, 4000),
-    #                                          nn.ReLU(),
-    #                                          # nn.Dropout(0.2),
-    #                                          nn.Linear(4000, 1280),
-    #                                          nn.ReLU(),
-    #                                          nn.Linear(1280, 102),
-    #                                          nn.LogSoftmax(dim=1))
-    #         state_dict = torch.load(checkpoint)
-    #         model.class_to_idx = train_data.class_to_idx
-    #         print(model.class_to_idx)
-    #         model.load_state_dict(state_dict)
-    #         print(model)
-    #         return model
-    #
-    #     loadedModel = rebuild('checkpoint.pth')
-
+    def load_model_checkpoint(self, checkpoint):
+        # TODO: Write a function that loads a checkpoint and rebuilds the model
+        from torchvision import datasets, transforms, models
+        def rebuild(checkpoint):
+            model = models.vgg11(pretrained=True)
+            model.classifier = nn.Sequential(nn.Linear(512 * 7 * 7, 4000),
+                                             nn.ReLU(),
+                                             # nn.Dropout(0.2),
+                                             nn.Linear(4000, 1280),
+                                             nn.ReLU(),
+                                             nn.Linear(1280, 102),
+                                             nn.LogSoftmax(dim=1))
+            state_dict = torch.load(checkpoint)
+            model.class_to_idx = train_data.class_to_idx
+            print(model.class_to_idx)
+            model.load_state_dict(state_dict)
+            print(model)
+            return model
+    
+        loadedModel = rebuild('checkpoint2.pth')
+        return loadedModel
 
 ####################
 # Examples of usage
@@ -234,18 +263,19 @@ class Model:
 #
 # Use GPU for Training:
 # python train.py data_dir --gpu
+
 def main():
     parser = argparse.ArgumentParser(description="Caclulate x to the power of Y")
     parser.add_argument('data_dir', help="path to image dataset")
     parser.add_argument("-s", "--save_dir", default=".", help="path to saved model")
     parser.add_argument("-a", "--arch", default="vgg13", help="architecture of model")
-    parser.add_argument("-r", "--learning_rate", type=float, default="0.01", help="learning rate as float. Default: 0.01.")
+    parser.add_argument("-r", "--learning_rate", type=float, default=0.03, help="learning rate as float. Default: 0.01.")
     parser.add_argument("--hidden_units", type=int, default="512", help="Number of hidden units. Default: 512.")
     parser.add_argument("-e", "--epochs", type=int, default="20", help="Number of epochs. Default: 20")
     parser.add_argument("--gpu", action='store_true', help="Turn on Cuda Usage")
 
     args = parser.parse_args()
-    print(args.data_dir)
+    print('data directory set to "{}".'.format(args.data_dir))
     myModel = Model(args.data_dir,
                     save_dir=args.save_dir,
                     arch=args.arch,
@@ -255,8 +285,11 @@ def main():
                     gpu=args.gpu,)
 
     print(myModel)
-    myModel.setModel()
+    myModel.setModel(myModel.arch)
     myModel.create_classifier()
+    myModel.train_model()
+    myModel.validate_model()
+    myModel.save_model_checkpoint()
 
 
 if __name__ == "__main__":
